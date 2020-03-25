429838472e1a643245d8c16fc0ce2dd8005becc9
pmandrik <petr.mandrik@cern.ch>
2020-03-25 03:59:48 +0300

    minor fixes

--- pmlib_v2d_py.py
+++ pmlib_v2d_py.py
@@ -13,16 +13,23 @@ class v2():
   def __init__(self, x=0, y=0, z=0):
     self.x, self.y, self.z = x, y, z
 
-  def Rotate(self, anlgle):
+  def Rotate(self, angle):
     c, s = cos(angle), sin(angle)
-    return v2( self.x * c -  self.y * s,  self.x * s +  self.y * c )
+    return v2( self.x * c + self.y * s,  - self.x * s +  self.y * c )
 
   def Rotated(self, angle):
     return self.Rotate(angle * PI_180)
 
-  def L(): return hypot(x, y)
+  def L(self):
+    return hypot(self.x, self.y)
 
-  def L2(): return x*x + y*y
+  def L2(self):
+    return self.x*self.x + self.y*self.y
+
+  def AddList(self, other) : 
+    self.x += other[0]
+    self.y += other[1]
+    return self
 
   def Tuple(self):
     return (self.x, self.y)
@@ -41,13 +48,15 @@ class v2():
     self.x /= val
     self.y /= val
 
-  def __iadd__(self, other):
+  def __iadd__(self, val):
     self.x += val.x
     self.y += val.y
+    return self
 
-  def __isub__ (self, other):
+  def __isub__ (self, val):
     self.x -= val.x
     self.y -= val.y
+    return self
 
   def __mul__(self, val):
     return v2(self.x * val, self.y * val)
@@ -65,21 +74,25 @@ class v2():
     if self.x == other.x and self.y == other.y : return True
     return False
 
+  def __hash__(self):
+    return self.x * 1000000000000 + self.y
+
   def __ne__(self, other):
     if self.x != other.x and self.y != other.y : return True
     return False
 
-  def __neg__(self) :
-    return Vector2D(-self.x, -self.y)
+  def __neg__(self): 
+    return v2(-self.x, -self.y)
 
   def __str__(self):
-    return "v2(%d, %d)"%(self.x, self.y)
+    return "v2(" + str(self.x) + ", " + str(self.y)
+
+  def __len__(self):
+    return 2
 
   def __getitem__(self, key):
     if key == 0 : return self.x
     if key == 1 : return self.y
     if key == 2 : return self.z
-
-
-
+    return False
 
