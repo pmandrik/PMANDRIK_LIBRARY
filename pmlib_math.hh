9ae2466c795c681dca7b3752135bedf0e099e5a9
pmandrik <petr.mandrik@cern.ch>
2023-04-17 23:40:03 +0300

    add pmlib_math

---
+++ pmlib_math.hh
@@ -0,0 +1,15 @@
+// P.~Mandrik, 2021, https://github.com/pmandrik
+
+#ifndef PMLIB_MATH_HH
+#define PMLIB_MATH_HH 1 
+
+namespace pm {
+
+  const float PI = 3.14159265358979323846;
+  const float OSML = 0.00000000000001;
+  const float PI_180 = PI/180.;
+  const float IP_081 = 180./PI;
+
+};
+
+#endif
